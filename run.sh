396fcc320a88114c89cda71c1c71af45536f1ffd
Falco <falco@falco.bz>
2017-07-29 02:57:27 +0200

    improve run.sh wrt postmap

--- run.sh
+++ run.sh
@@ -4,7 +4,12 @@ touch /var/log/maillog
 touch /var/log/messages
 chmod o+r /var/log/*
 
-postmap /etc/postfix/user/saslpass
+pushd /etc/postfix/user/
+for f in `find -type f`; do
+    c=`awk '/^#run:/ {gsub (/^#run:/,""); print $0}' $f`
+    [ -z "$c" ] || ( echo + $c $f && eval $c $f )
+done
+popd
 
 /usr/bin/supervisord
 
